f03942aaed5b295c0282ba78a6b7b154567769c9
Naniz11 <naniz110504@gmail.com>
2023-06-23 17:38:05 -0500

    feat: :sparkles: INSERTAMOS EL CRUD

--- scripts/db/connect.php
+++ scripts/db/connect.php
@@ -1,14 +1,15 @@
 <?php
 namespace App;
 class connect extends credentials{
-    protected $con;
+    public $con;
     function __construct(private $dns = "mysql", private $port = 3306){
        try {
          $this->con=new \PDO($this->dns.":host=".$this->__get('host').";dbname=".$this->__get('dbname').";user=". $this->username.";password=".$this->__get('password').";port=". $this->port);
-    print_r("xdd");
+   // print_r("xdd");
        } catch(\PDOException $e){
         print_rt($e->getMessage());
        }
-    }
+       
+    } 
 }
 ?>
--- uploads/app.php
+++ uploads/app.php
@@ -1,6 +1,45 @@
 <?php 
 require "../vendor/autoload.php";
-new \App\connect();
+$router =  new \Bramus\Router\Router();
 
+$router->get("/camper", function(){
+    // SELECT *FROM tb_camper;
+    $cox = new \App\connect();
+    $res = $cox->con->prepare("SELECT * FROM tb_camper");
+    $res->execute();
+    $res = $res->fetchAll(\PDO::FETCH_ASSOC);
+    echo json_encode($res);
+});
+$router->put("/camper", function(){
+    $_DATA = json_decode(file_get_contents('php://input'), true);
+    $cox = new \App\connect();
+    $res = $cox->con->prepare("UPDATE tb_camper SET nombre = :NOMBRE WHERE ID=:CEDULA");
+    $res->bindValue("NOMBRE", $_DATA["nom"]);
+    $res->bindValue("CEDULA", $_DATA["id"]);
+    $res->execute();
+    $res= $res->rowCount();
+    echo json_encode($res);
+});
+$router->delete("/camper", function(){
+    $_DATA = json_decode(file_get_contents('php://input'), true);
+    $cox = new \App\connect();
+    $res = $cox->con->prepare("DELETE FROM  tb_camper WHERE id=:ID");
+    $res->bindValue("ID", $_DATA["id"]);
+    $res->execute();
+    $res= $res->rowCount();
+    echo json_encode($res);
+});
+$router->post("/camper", function(){
+    $_DATA = json_decode(file_get_contents('php://input'), true);
+    $cox = new \App\connect();
+    $res = $cox->con->prepare("INSERT INTO tb_camper (nombre,edad) VALUES (:nom, :edad");
+    $res->bindValue("nom", $_DATA["nom"]);
+    $res->bindValue("edad", $_DATA["edad"]);
+    $res->execute();
+    $res= $res->rowCount();
+    echo json_encode($res);
+});
+
+$router->run();
 
 ?>
